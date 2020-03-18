33c8dcc7c637cd63f46b65bd9c150d8ea75a5f73
Gareth Leachman <grleachman@yahoo.com>
2020-03-18 22:58:59 +0000

    Complexity of column order

--- samples/BeforeWebForms/ControlSamples/GridView/Customer.cs
+++ samples/BeforeWebForms/ControlSamples/GridView/Customer.cs
@@ -1,13 +1,20 @@
-﻿namespace BeforeWebForms.ControlSamples.GridView
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BeforeWebForms.ControlSamples.GridView
 {
-  public class Customer
+	public class Customer
 	{
+		[Display(Name = "Id", Order = 7)]
 		public int CustomerID { get; set; }
 
+		[Display(Name = "FirstName", Order = 6)]
+
 		public string FirstName { get; set; }
+		[Display(Name = "LastName", Order = 3)]
 
 		public string LastName { get; set; }
 
+		[Display(Name = "Company", Order = 5)]
 		public string CompanyName { get; set; }
 	}
 }
